21e5945c82dd75a0236712509b487ee6f83ee0b6
肖保强 <xiaobq@shuhaisc.com>
2026-02-06 15:52:59 +0800

    使用chatglm tokenizer处理对话数据

--- nanochat/cust_dataset.py
+++ nanochat/cust_dataset.py
@@ -1,89 +1,225 @@
 import functools
 import json
+from collections.abc import Mapping, Sequence
 
 import torch
-from datasets import Dataset
-# 可能需要特殊结构
-from torch.nn.utils.rnn import pad_sequence
+from datasets import load_dataset
 from torch.utils.data import DataLoader
+from transformers import DataCollatorForSeq2Seq as _DataCollatorForSeq2Seq
+from transformers import PreTrainedTokenizer, DataCollatorForSeq2Seq
 
 device = "cuda" if torch.cuda.is_available() else "cpu"  # 检测是否有GPU，如果有则使用，否则使用CPU
 
 
-def prepare_for_dialogue(examples, tokenizer, block_size):
+# 可能需要特殊结构
+def prepare_for_dialogue(examples):
     """对话任务处理"""
     processed = []
     for doc in examples['text']:
         if "NaN" in doc:
             doc = doc.replace("NaN", "\"？\"")
         conversations = json.loads(doc)
-        tokens = []
-        for turn in conversations['conversations']:
-            if turn['from'] == 'user':
-                tokens.extend(tokenizer.encode(f"User: {turn['value']}"))
+        processed.append(conversations['conversations'])
+    return {'conversations': processed}
+
+
+def process_batch(
+        batch: Mapping[str, Sequence],
+        tokenizer: PreTrainedTokenizer,
+        max_input_length: int,
+        max_output_length: int,
+) -> dict[str, list]:
+    batched_conv = batch['conversations']
+    batched_input_ids = []
+    batched_labels = []
+
+    for conv in batched_conv:
+        input_ids, loss_masks = [tokenizer.get_command('[gMASK]'), tokenizer.get_command('sop')], \
+                                [False, False]
+        for message in conv:
+            if message['from'] in ('system', 'user'):
+                loss_mask_val = False
             else:
-                tokens.extend(tokenizer.encode(f"Assistant: {turn['value']}"))
-            tokens.append(tokenizer.eos_token_id)
-        processed.extend(tokens)
+                loss_mask_val = True
 
-    total = len(processed)
-    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-    # customize this part to your needs.
-    total_length = 0
-    if total >= block_size:
-        total_length = (total // block_size) * block_size
-    if total - total_length > 0:
-        total_length = total_length + 1
-    batch = [processed[i: i + block_size] for i in range(0, total_length, block_size)]
+            new_input_ids = tokenizer.build_single_message(
+                message['from'], '', message['value']
+            )
+            new_loss_masks = [loss_mask_val] * len(new_input_ids)
 
-    return {'input_ids': batch}
+            input_ids += new_input_ids
+            loss_masks += new_loss_masks
+
+        input_ids.append(tokenizer.eos_token_id)
+        loss_masks = [False, *loss_masks]
+        labels = []
+        for input_id, mask in zip(input_ids, loss_masks):
+            if mask:
+                labels.append(input_id)
+            else:
+                labels.append(-100)
+        max_length = max_input_length + max_output_length + 1
+        batched_input_ids.append(input_ids[:max_length])
+        batched_labels.append(labels[:max_length])
+    return {'input_ids': batched_input_ids, 'labels': batched_labels}
+
+
+def process_batch_eval(
+        batch: Mapping[str, Sequence],
+        tokenizer: PreTrainedTokenizer,
+        max_input_length: int,
+        max_output_length: int,
+) -> dict[str, list]:
+    batched_conv = batch['conversations']
+    batched_input_ids = []
+    # To avoid computing loss, we do not provide the `labels` field in the input dictionary.
+    batched_output_ids = []
+
+    for conv in batched_conv:
+        input_ids = [
+            tokenizer.get_command('[gMASK]'),
+            tokenizer.get_command('sop'),
+        ]
+        for message in conv:
+            if len(input_ids) >= max_input_length:
+                break
+            else:
+                new_input_ids = tokenizer.build_single_message(
+                    message['from'], '', message['value']
+                )
+                if message['from'] == 'assistant':
+                    output_prompt, output_ids = (
+                        new_input_ids[:1],
+                        new_input_ids[1:],
+                    )
+                    output_ids.append(tokenizer.eos_token_id)
+                    batched_input_ids.append(
+                        input_ids[:max_input_length] + output_prompt[:1]
+                    )
+                    batched_output_ids.append(output_ids[:max_output_length])
+                input_ids += new_input_ids
+    return {'input_ids': batched_input_ids, 'labels': batched_output_ids}
+
+
+def _sanity_check(
+        input_ids: Sequence[int],
+        output_ids: Sequence[int],
+        tokenizer: PreTrainedTokenizer,
+):
+    print('--> Sanity check')
+    for in_id, out_id in zip(input_ids, output_ids):
+        if in_id == 0:
+            continue
+        if in_id in tokenizer.tokenizer.index_special_tokens:
+            in_text = tokenizer.tokenizer.index_special_tokens[in_id]
+        else:
+            in_text = tokenizer.decode([in_id])
+        print(f'{repr(in_text):>20}: {in_id} -> {out_id}')
+
+
+class DataCollatorForSeq2Seq(_DataCollatorForSeq2Seq):
+    def _pad_tensors_to_max_len(self, tensor, max_length):
+        if self.tokenizer is not None and hasattr(self.tokenizer, "pad_token_id"):
+            # If PAD token is not defined at least EOS token has to be defined
+            pad_token_id = (
+                self.tokenizer.pad_token_id
+                if self.tokenizer.pad_token_id is not None
+                else self.tokenizer.eos_token_id
+            )
+        else:
+            if self.model.config.pad_token_id is not None:
+                pad_token_id = self.model.config.pad_token_id
+            else:
+                raise ValueError("Pad_token_id must be set in the configuration of the model, in order to pad tensors")
+
+        padded_tensor = pad_token_id * torch.ones(
+            (tensor.shape[0], max_length), dtype=tensor.dtype, device=tensor.device
+        )
+        padded_tensor[:, : tensor.shape[-1]] = tensor
+        return padded_tensor
+
+    def __call__(self, features, return_tensors=None):
+        batch = super().__call__(features, return_tensors)
+        src_len = batch['input_ids'].size(-1)
+        tgt_len = batch['labels'].size(-1)
+        max_len = max(src_len, tgt_len)
+        batch['input_ids'] = self._pad_tensors_to_max_len(batch['input_ids'], max_len)
+        batch['labels'] = self._pad_tensors_to_max_len(batch['labels'], max_len)
+        batch['labels'][batch['labels'] == self.tokenizer.pad_token_id] = -100
+        batch['input_ids'] = batch['input_ids'].to(device)
+        batch['labels'] = batch['labels'].to(device)
+        return batch
 
 
 def get_dataloader(args, tokenizer):
-    pad_token_id = tokenizer.pad_token_id
-    dataset = Dataset.from_text(args.train_data_file)
-    if args.eval_data_file is not None:
-        eval_dataset = Dataset.from_text(args.eval_data_file)
-    print("train dataset processing ...")
-    dataset = dataset.map(
-        functools.partial(
-            prepare_for_dialogue,
-            tokenizer=tokenizer,
-            block_size=args.max_seq_len + 1
-        ),
+    data_files = {'train': args.train_data_file, 'test': args.eval_data_file}
+    dataset = load_dataset('text', data_files=data_files)
+    train_dataset = dataset['train']
+    train_dataset = train_dataset.map(
+        prepare_for_dialogue,
         batched=True,
         batch_size=args.device_batch_size,
-        remove_columns=dataset.column_names,
+        remove_columns=train_dataset.column_names,
         load_from_cache_file=True,
         num_proc=args.num_workers
     )
-
-    if eval_dataset is not None:
-        print("using eval dataset, processing ...")
+    train_dataset = train_dataset.map(
+        functools.partial(
+            process_batch,
+            tokenizer=tokenizer,
+            max_input_length=args.max_seq_len,
+            max_output_length=args.max_seq_len
+        ),
+        batched=True,
+        num_proc=args.num_workers,
+        remove_columns=train_dataset.column_names,
+        load_from_cache_file=True,
+    )
+    print("train_dataset:")
+    _sanity_check(train_dataset[0]["input_ids"], train_dataset[0]["labels"], tokenizer)
+    print('=' * 100)
+    eval_dataset = None
+    if dataset['test'] is not None:
+        eval_dataset = dataset['test']
         eval_dataset = eval_dataset.map(
-            functools.partial(
-                prepare_for_dialogue,
-                tokenizer=tokenizer,
-                block_size=args.max_seq_len + 1
-            ),
+            prepare_for_dialogue,
             batched=True,
             batch_size=args.device_batch_size,
             remove_columns=eval_dataset.column_names,
             load_from_cache_file=True,
             num_proc=args.num_workers
         )
+        eval_dataset = eval_dataset.map(
+            functools.partial(
+                process_batch_eval,
+                tokenizer=tokenizer,
+                max_input_length=args.max_seq_len,
+                max_output_length=args.max_seq_len
+            ),
+            batched=True,
+            num_proc=args.num_workers,
+            remove_columns=eval_dataset.column_names,
+            load_from_cache_file=True,
+        )
+        print("eval_dataset:")
+        _sanity_check(eval_dataset[0]["input_ids"], eval_dataset[0]["labels"], tokenizer)
+        print('=' * 100)
 
-    def data_collator(examples):
-        ids = [torch.tensor(example['input_ids'], dtype=torch.long, device=device) for example in examples]
-        input_ids = pad_sequence(ids, batch_first=True, padding_value=pad_token_id)
-        labels = input_ids.clone()
-        input_ids = input_ids[:, :-1]
-        shifted = labels[:, 1:]
-        shifted[shifted == pad_token_id] = -100
-        return {'input_ids': input_ids, 'labels': shifted}
+    it = iter(train_dataset)
+    for i in range(5):
+        data = next(it)
+        print(tokenizer.decode(data['input_ids']))
+        print(''.join([tokenizer.decode([i]) if i != -100 else str(i) for i in data['labels']]))
+        print('-' * 100)
 
+    data_collator = DataCollatorForSeq2Seq(
+        tokenizer=tokenizer,
+        padding='longest',
+        max_length=args.max_seq_len,
+        return_tensors='pt',
+    )
     train_loader = DataLoader(
-        dataset,
+        train_dataset,
         batch_size=args.device_batch_size,
         collate_fn=data_collator
     )
